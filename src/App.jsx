4455a215c0196a84972e8e176d12be7f334d1ccd
pushkar <pushkardutt555@gmail.com>
2024-04-22 14:36:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [search, setSearch] = useState("");
+  const [currentWeather, setCurrentWeather] = useState("");
+
+  const searched = () => {
+    fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
+      .then((res) => res.json())
+      .then((result) => {
+        // console.log(result);
+        setCurrentWeather(result);
+      });
+  };
+  const api = {
+    key: "e5bfc32995db55adf3b136208cb86e7e",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
+
+  return (
+    <>
+      <h1>Weather App with React</h1>
+      <input
+        className="mt-10 w-64 h-12 px-4 py-2 rounded-md bg-slate-50 text-black"
+        type="text"
+        placeholder="City?"
+        onChange={(e) => setSearch(e.target.value)}
+      ></input>
+
+      <button onClick={searched} className="ml-4 bg-red-500">
+        Search
+      </button>
+      {typeof currentWeather.main !== "undefined" ? (
+        <>
+          <h1 className="mt-8">{currentWeather.name}</h1>
+          <p className="mt-8 text-orange-400">{currentWeather.main.temp}°C</p>
+          <p className="mt-8">{currentWeather.weather[0].main}</p>
+        </>
+      ) : null}
+    </>
+  );
+}
+
+export default App;
